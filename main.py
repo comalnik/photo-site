bcf49894bc9fddd6d9f1b5ef80753704773514ab
Luka Gostenčnik <gostencnikluka@proton.me>
2024-08-21 15:53:53 +0200

    style improvements, exception handling

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import glob
 import os
 import piexif
 from PIL import Image
+from PIL import ImageFile
 from PIL.ExifTags import TAGS
-import imghdr
+import exifread
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 MAXSIZE = 1000
-EXIFPARAMS = "Artist", "Make", "Model", "Software", "DateTimeOriginal", "ShutterSpeedValue", "ApertureValue", "BrightnessValue", "FocalLength", "ExifImageWidth", "ExifImageHeight", "FocalPlaneXResolution", "FocalPlaneYResolution", "ExposureTime", "FNumber", "ISOSpeedRatings", "LensMake", "LensModel", "ImageWidth", "ImageLength", "FocalLengthIn35mmFilm"
-
+EXIFPARAMS = "Make", "Model", "Software", "DateTimeOriginal", "ShutterSpeedValue", "ApertureValue", "BrightnessValue", "FocalLength", "ExifImageWidth", "ExifImageHeight", "ExposureTime", "FNumber", "ISOSpeedRatings", "LensMake", "LensModel", "ImageWidth", "ImageLength","Artist", "FocalLengthIn35mmFilm"
 
 
 
@@ -33,18 +35,16 @@ def home():
         resized_image = image.resize((new_width, new_height))
         resized_image.save(output_path, optimize=True, quality=80)
 
-
-        
-        #removes gps data from jpeg images
-        for i in images:
-            if imghdr.what(cdpath+'/static/images/'+i) != "jpeg":
-                pass
-            else:
-                img = piexif.load(cdpath+'/static/images/'+i)
-                if 'GPS' in img:
-                    del img['GPS']
-                    exif_bytes = piexif.dump(img)
-                    piexif.insert(exif_bytes, cdpath+'/static/images/'+i)
+    #remove gps data
+    for i in images:
+        try:
+            img = piexif.load(cdpath+'/static/images/'+i)
+            if 'GPS' in img:
+                del img['GPS']
+                exif_bytes = piexif.dump(img)
+                piexif.insert(exif_bytes, cdpath+'/static/images/'+i)
+        except:
+            pass
 
     #checks for new images, and makes thumbnails
     if set(images) != set(thumbs):
@@ -54,14 +54,29 @@ def home():
             savedir = thumbsdir + i
             resize_image(imgdir, savedir, maxsize)
 
+
     thumbslater = [os.path.basename(x) for x in glob.glob(cdpath+"/static/thumbs/*")]
-    return render_template("index.html", imges=thumbslater)
+    high = []
+    wide = []
+    for i in thumbslater:
+        image = Image.open(cdpath + "/static/thumbs/" + i)
+        width, height = image.size
+        if width > height:
+            wide.append(i)
+        else:
+            high.append(i)
+
+    
+
+    return render_template("index.html", wide=wide, high=high)
 
 
 @app.route("/<image>")
 def image(image):
     cdpath = os.path.dirname(os.path.realpath(__file__))
     image_path = cdpath + "/static/images/" + image
+    
+
     def get_image_exif(image_path):
         image = Image.open(image_path)
         exif_data = image._getexif()
@@ -72,17 +87,19 @@ def image(image):
         
         for tag_id, value in exif_data.items():
             tag_name = TAGS.get(tag_id, tag_id)
-            if  len(str(value)) <= 150:
-                exif_list.append((tag_name, value))
+            
+            exif_list.append((tag_name, value))
         
         return exif_list
     try:
         exif_data = get_image_exif(image_path)
-        #exif data filtering
         exif_data_list = [(tag_name, value) for tag_name, value in exif_data if tag_name in (EXIFPARAMS)]
     except:
         exif_data_list = ""
+
+    
+
     return render_template("image.html", link=image, metadata=exif_data_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
